6bceac4d6212342a0f0026179730a20d6975f134
CRMyers72 <91919116+CRMyers72@users.noreply.github.com>
2022-01-23 16:59:44 -0500

    finalized project

---
+++ build/precache-manifest.8e5659b4b9f765b9bd0b21fa0b30e596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d3e8da10ca3e3479437af832dba936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8adb3a1cab9e6012fcc7",
+    "url": "/static/js/2.6c99d600.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c99d600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "320ff6aca3249d3b2e24",
+    "url": "/static/js/main.77135bfc.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,20 @@
 /**
  * The index.js file is used to re-export from our separate files, that way rather than write:
- * 
+ *
  * import Feature from './components/Feature';
- * 
+ *
  * we can write:
- * 
+ *
  * import { Feature } from './components';
- * 
+ *
  * since index.js is assumed as part of the import when you specify a folder
- * 
+ *
  * Re-export Feature, Loading, Preview, Search, and Title from their respective files
  */
+import Feature from "./Feature";
+import Loading from "./Loading";
+import Search from "./Search";
+import Title from "./Title";
+import Preview from "./Preview";
+
+export { Feature, Loading, Search, Title, Preview };
